c4175e7b789578c18ceeed1c26ae56a1da468cfa
John Hughes <john.hughes@musictribe.com>
2024-08-03 12:07:39 +0100

    refactor: :recycle: enable producer Dockerfile to build correctly

--- producer/producer.go
+++ producer/producer.go
@@ -1,8 +1,11 @@
 package main
 
-import "log"
+import (
+	"github.com/sirupsen/logrus"
+)
 
 func main() {
 	// Set a k8s secret periodically
-	log.Println("+++ Producer started +++")
+	logger := logrus.New()
+	logger.Info("+++ Producer started +++")
 }
